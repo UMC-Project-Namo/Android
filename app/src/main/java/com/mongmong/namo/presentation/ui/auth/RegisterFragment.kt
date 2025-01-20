ce70e209e7bbff5dda7c5c58487cf0d431bd6f5a
edv-Shin <edvedv613@gmail.com>
2025-01-20 18:36:01 +0900

    ✨ Feat: 회원가입 화면 레이아웃 구현
    Related to: #350

---
+++ app/src/main/java/com/mongmong/namo/presentation/ui/auth/RegisterFragment.kt
@@ -0,0 +1,16 @@
+package com.mongmong.namo.presentation.ui.auth
+
+import androidx.fragment.app.Fragment
+import com.mongmong.namo.R
+import com.mongmong.namo.databinding.FragmentRegisterBinding
+import com.mongmong.namo.presentation.config.BaseFragment
+
+class RegisterFragment : BaseFragment<FragmentRegisterBinding>(R.layout.fragment_register) {
+    override fun setup() {
+        getName()
+    }
+
+    fun getName() {
+
+    }
+}
